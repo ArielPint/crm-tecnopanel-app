50e1fd6f1d68a0bbbb16fa829d95401943c6386b
ArielPint <103214866+ArielPint@users.noreply.github.com>
2026-06-27 17:17:59 -0400

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: ['class'],
+  content: ['./index.html', './src/**/*.{ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          red:   '#ed3224',
+          dark:  '#c0241a',
+          gray:  '#424243',
+          bg:    '#1a1a1b',
+        },
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+    },
+  },
+  plugins: [require('tailwindcss-animate')],
+}
